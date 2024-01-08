e77fd86899676c4de504e7c91d194f79525eab7c
MehmetCan Özoğlu <mehmetcanozoglu1@gmail.com>
2024-01-08 18:13:21 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,64 @@
+// let skillsSection = document.getElementById("skills-section")
+// let progressBars = document.querySelectorAll(".progress-bar")
+
+// function showProgress(){
+//     progressBars.forEach(progressBars => {
+//         let value = progressBars.dataset.progress;
+
+//         progressBars.style.opacity = 1;
+//         progressBars.style.opacity = 1;
+//         progressBars.style.width = `${value}%`;
+//     })
+// }
+
+// function hideProgress(){
+//     progressBars.forEach(p =>{
+//         p.style.opacity = 0;
+//         p.style.width = 0;
+//     })
+// }
+
+// window.addEventListener('scroll', () => {
+//     let sectionPos = skillsSection.getBoundingClientRect().top;
+//     let screenPos = window.innerHeight / 2;
+
+//     if(sectionPos < screenPos){
+//        showProgress()
+//     }
+// })
+
+// let yukari = document.getElementsByClassName('yukari_cik')[0];
+// //console.log(yukarı);
+
+// window.addEventListener('scroll', function(){
+//     let mesafe = window.scrollY;
+//     // console.log(mesafe);//yukarıdan olan uzaklıgı verir
+//     if(mesafe > 300){
+//      yukari.classList.add("göster");
+//     }else{
+//         yukari.classList.remove('göster')
+//     }
+// });
+
+ 
+
+// yukari.addEventListener('click', function(){
+//     window.scrollTo(0,0)
+
+
+// }) 
+
+// const button = document.querySelector('.snowboard_img');
+
+// // Add a mouseover event listener
+// button.addEventListener('mouseover', () => {
+//   // Change the button's background color
+//   button.style.background = 'blue';
+// });
+
+// // Add a mouseout event listener
+// button.addEventListener('mouseout', () => {
+//   // Change the button's background color back to its original color
+//   button.style.background = '';
+// });
+
